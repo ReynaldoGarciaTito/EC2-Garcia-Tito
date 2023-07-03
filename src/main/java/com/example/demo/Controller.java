cabeab365752220109568b3cc01876de0cd9300b
ReynaldoGarciaTito <134290492+ReynaldoGarciaTito@users.noreply.github.com>
2023-07-03 13:03:28 +0000

    Cambios1

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(path="/")
+public class Controller{
+
+    @GetMapping(path="/")
+    public String home(){
+        return "SM75716675 - EC2 - Garcia Tito";
+    }
+}
